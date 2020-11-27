03b1d9ee9d4f68120279c8fea1e6a39c099d64e9
Revertigo <dekel241@gmail.com>
2020-11-27 15:12:03 +0200

    Move Mover script into folder

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -25,13 +25,4 @@ public class Mover : MonoBehaviour
             //col.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-2f, 2f), Random.Range(1, 4f)).normalized * PlayerPrefs.GetInt("speed");
         }
     }
-
-    //Called whenever a trigger has entered this objects BoxCollider2D. The value 'col' is the Collider2D object that has interacted with this one
-    /*	void OnTriggerEnter2D(Collider2D col)
-        {
-            if (col.gameObject.tag == "Ball")
-            {                                           //Is the colliding object got the tag "Ball"?
-                col.gameObject.GetComponent<Ball1>().SetDirection(transform.position);   //Get the 'Ball' component of the colliding object and call the 'SetDirection()' function to bounce the ball of the paddle
-            }
-        }*/
 }
