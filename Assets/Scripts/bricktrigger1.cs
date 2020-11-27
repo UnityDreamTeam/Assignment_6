384edc64d3b3740b7ae94ee4162314dc3c186764
DESKTOP-K3BSLJJ\Maor <maor.ovadia6@gmail.com>
2020-11-27 20:14:21 +0200

    Add ball collision

--- Assets/Scripts/bricktrigger1.cs
+++ Assets/Scripts/bricktrigger1.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class bricktrigger1 : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
 	}
 	
@@ -13,15 +14,29 @@ public class bricktrigger1 : MonoBehaviour {
 	
 	}
 
-	void OnTriggerEnter2D(Collider2D col)
-	{
-		if(col.gameObject.tag=="laser")
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+		if (collision.gameObject.tag == "ball")
 		{
-			Destroy(col.gameObject);
-			PlayerPrefs.SetInt("count", PlayerPrefs.GetInt("count")+1);
-			PlayerPrefs.SetInt("score", PlayerPrefs.GetInt("score")+100);
+			PlayerPrefs.SetInt("count", PlayerPrefs.GetInt("count") + 1);
+			PlayerPrefs.SetInt("score", PlayerPrefs.GetInt("score") + 100);
 			PlayerPrefs.Save();
 			Destroy(gameObject);
 		}
+
+		if (collision.gameObject.tag == "laser")
+		{
+			Destroy(collision.gameObject);
+			PlayerPrefs.SetInt("count", PlayerPrefs.GetInt("count") + 1);
+			PlayerPrefs.SetInt("score", PlayerPrefs.GetInt("score") + 100);
+			PlayerPrefs.Save();
+			Destroy(gameObject);
+		}
+
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+	{
+		
 	}
 }
