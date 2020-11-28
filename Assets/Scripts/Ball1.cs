f1a821e6a5ec8ddbaa21799d5ae18453a09e29e7
Revertigo <dekel241@gmail.com>
2020-11-28 02:28:01 +0200

    Remove hard coded number to indices

--- Assets/Scripts/Ball1.cs
+++ Assets/Scripts/Ball1.cs
@@ -26,7 +26,10 @@ public class Ball1 : MonoBehaviour {
 	public Text gscore;
 	float y1=0;
 	float y2=0;
+
 	AudioSource[] audioSource;
+	readonly int player_sound = 0;
+	readonly int brick_sound = 1;
 
 	[SerializeField] protected KeyCode keyToPress;
 	[SerializeField] int speed;
@@ -95,7 +98,7 @@ public class Ball1 : MonoBehaviour {
 	{
 		if (col.gameObject.tag=="Player") 
 		{
-			audioSource[0].Play();
+			audioSource[player_sound].Play();
 			//GetComponent<Rigidbody2D>().velocity =  new Vector2(Random.Range(-2f,2f), Random.Range(1,4f)).normalized*PlayerPrefs.GetInt("speed");
 
 			float delta_x = col.gameObject.GetComponent<Transform>().position.x - GetComponent<Transform>().position.x;
@@ -113,7 +116,7 @@ public class Ball1 : MonoBehaviour {
 			PlayerPrefs.SetInt("score", PlayerPrefs.GetInt("score")+100);
 			PlayerPrefs.SetInt("count", PlayerPrefs.GetInt("count")+1);
 			PlayerPrefs.Save();
-			audioSource[1].Play();
+			audioSource[brick_sound].Play();
 			if(ballcount==(int)Random.Range(0,50))
 			{
 				 Instantiate(balls, new Vector3(transform.position.x, transform.position.y), Quaternion.identity);
